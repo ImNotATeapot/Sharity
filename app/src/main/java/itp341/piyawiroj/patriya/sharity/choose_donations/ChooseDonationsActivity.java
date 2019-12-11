123fe70806d7fdf252986cd2b3bbf08759ff3be6
ImNotATeapot <33531627+ImNotATeapot@users.noreply.github.com>
2019-12-10 18:59:51 -0800

    Firebase Storage

--- app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/ChooseDonationsActivity.java
+++ app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/ChooseDonationsActivity.java
@@ -1,12 +1,19 @@
 package itp341.piyawiroj.patriya.sharity.choose_donations;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,9 +27,10 @@ public class ChooseDonationsActivity extends AppCompatActivity {
 
     private static final String TAG = ChooseDonationsActivity.class.getSimpleName();
     public TextView title;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_tabbed_activity);
         DonationSectionsPagerAdapter sectionsPagerAdapter = new DonationSectionsPagerAdapter(getSupportFragmentManager());
@@ -40,7 +48,27 @@ public class ChooseDonationsActivity extends AppCompatActivity {
                 finish();
             }
         });
-
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        signInAnonymously();
+    }
+    private void signInAnonymously() {
+        mAuth.signInAnonymously().addOnSuccessListener(this, new  OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Log.d(TAG,"SUSSESS");
+                // do your stuff
+            }
+        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.e(TAG, "signInAnonymously:FAILURE", exception);
+                    }
+                });
     }
 
     private class DonationSectionsPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/SearchFragment.java
+++ app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/SearchFragment.java
@@ -1,46 +1,76 @@
 package itp341.piyawiroj.patriya.sharity.choose_donations;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+it
+import java.net.URI;
 
 import itp341.piyawiroj.patriya.sharity.R;
 
 public class SearchFragment extends Fragment {
-    @Nullable
 
-    TextView a;
+    private static final String TAG = SearchFragment.class.getSimpleName();
+
+    @Nullable
+    ImageView a;
     DatabaseReference reff;
+    StorageReference storageRef;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.choose_donations_search, container, false);
-        reff = FirebaseDatabase.getInstance().getReference().child("Member").child("1");
-        reff.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.child("Name").getValue().toString();
-                String location = dataSnapshot.child("Location").getValue().toString();
-                String print = name+location;
-                a.setText(print);
-            }
+        reff = FirebaseDatabase.getInstance().getReference().child("0").child("imageid");
+        storageRef  = FirebaseStorage.getInstance().getReference().child("DMC.jpeg");
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        a = v.findViewById(R.id.testTextView);
+//        final StorageReference pathReference = storageRef.child("DWC.jpeg");
+//        storageRef.child("DWC.jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                // Got the download URL for 'users/me/profile.png'
+//                Glide.with(getActivity()).load(pathReference).into(a);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                // Handle any errors
+//            }
+//        });
+//
+//        reff.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String name = dataSnapshot.getValue().toString();
+//                //String location = dataSnapshot.child("Location").getValue().toString();
+//                //String print = name+location;
+//                Glide.with(getActivity()).load(storageRef).into(a);
+//                //a.setText(print);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+        //a = v.findViewById(R.id.testTextView);
         return v;
     }
 }
