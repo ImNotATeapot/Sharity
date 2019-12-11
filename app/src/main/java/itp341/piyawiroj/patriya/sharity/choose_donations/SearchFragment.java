1c5eee99ec335b1447268236d63c85a6f2f2a6d1
ImNotATeapot <33531627+ImNotATeapot@users.noreply.github.com>
2019-12-10 23:05:12 -0800

    minor ui fixes

--- app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/SearchFragment.java
+++ app/src/main/java/itp341/piyawiroj/patriya/sharity/choose_donations/SearchFragment.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-it
+
 import java.net.URI;
 
 import itp341.piyawiroj.patriya.sharity.R;
--- app/src/main/java/itp341/piyawiroj/patriya/sharity/main/MainActivity.java
+++ app/src/main/java/itp341/piyawiroj/patriya/sharity/main/MainActivity.java
@@ -1,12 +1,12 @@
 package itp341.piyawiroj.patriya.sharity.main;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 // check for permissions
                 if ( ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
                     //request permissions
-                    String[] permissionsList = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+                    String[] permissionsList = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION};
                     requestPermissions(permissionsList, 0);
                 } else {
                     Intent i = new Intent(getApplicationContext(), ChooseDonationsActivity.class);
@@ -73,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(), ChooseDonationsActivity.class);
                 i.putExtra(DonationCenter.EXTRA_LOCATION, zip);
                 startActivity(i);
+
+//                if ( ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACTION_DIAL) != PackageManager.PERMISSION_GRANTED ) {
+//                    String dial = "tel:" + "7742903400";
+//                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+//                }
             }
         });
     }
